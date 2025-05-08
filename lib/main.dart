42f7cc2866c824c10935abba64867ff6bc0610c1
EmanuelC16-bit <66982258+EmanuelC16-bit@users.noreply.github.com>
2025-05-07 23:35:33 -0600

    Removed HomePage from main page (moved to PerformLogin)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provieasy_proveedores_main/pages/HomePage.dart';
+// import 'package:provieasy_proveedores_main/pages/HomePage.dart';
 import 'package:provieasy_proveedores_main/services/Connection.dart';
 
 void main() {
